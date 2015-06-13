1d59136e16e2cc143eeae6b2eda39a12504859c1
SGenheden <s.genheden@soton.ac.uk>
2015-06-13 20:51:06 +0100

    Put md processor logic of md_bondang in specialised classes for future reusability

--- md_bondang.py
+++ md_bondang.py
@@ -20,24 +20,18 @@ If an additional structure is given it is assumed that the trajectory
 is AA and this structure is used to read the MD trajectory as well
 as to setup the transformation from the AA to the CG representation.
 
-A current restriction is that the molecules to be tracked is of a
-single kind, e.g. a single lipid type in a membrane.
-
 Examples
 --------
 md_bondang.py -f prod.dcd -r popc_elba.pdb -x elba.xml
 md_bondang.py -f md2_whole.xtc -s md1.gro -r popc_elba.pdb -x elba.xml
 """
 
-import argparse
 import sys
 
-import MDAnalysis as md
 import MDAnalysis.core.util as mdutil
 import numpy as np
 
-import elbalib
-
+import mdlib
 
 def _calc_bond(xyz):
     return mdutil.norm(xyz[0, :] - xyz[1, :])
@@ -49,95 +43,35 @@ def _calc_angle(xyz):
     return np.rad2deg(np.arccos(np.dot(a, b) / (mdutil.norm(a) * mdutil.norm(b))))
 
 
+def _process_res(residue,processor,**kwargs) :
+    """
+    Called for each residue at every time step of the trajectory
+    Print out the bond and angles to the output file
+    """
+    f = kwargs["fileobj"]
+    f.write("%d"%processor.currtime)
+    for i, bondsel in enumerate(residue.bonds):
+        f.write("%8.3f" % _calc_bond(residue.xyz[bondsel, :]))
+    for i, angsel in enumerate(residue.angles):
+        f.write("%8.3f" % _calc_angle(residue.xyz[angsel, :]))
+    f.write("\n")
+
+
 if __name__ == '__main__':
 
     print " ".join(sys.argv)
 
-    # Command-line input
-    parser = argparse.ArgumentParser(description="Track CG bond and angle distributions through an MD trajectory")
-    parser.add_argument('-f', '--file', nargs="+", help="the trajectory file.", default="")
-    parser.add_argument('-r', '--ref', help="a CG reference file", default="ref.pdb")
-    parser.add_argument('-s', '--struct', help="a AA structure file")
-    parser.add_argument('-x', '--xml', help="an XML file with force field definitions")
-    parser.add_argument('-o', '--out', help="the output", default="bondang.txt")
-    parser.add_argument('-n', '--naming', nargs=2, help="naming convention change")
-    parser.add_argument('--skip', type=int, help="skip this many snapshots", default=0)
-    parser.add_argument('--dt', type=float, help="the number of ps for each snapshot", default=10)
-    args = parser.parse_args()
+    # Setup the trajectory processor
+    processor = mdlib.AaCgTrajectoryProcessor("Track CG bond and angle distributions through an MD trajectory")
+    processor.argparser.add_argument('-o', '--out', help="the output", default="bondang.txt")
+    
+    processor.setup()
+    # Add bond and angle indices to each of the defined residues
+    for res in processor.residues :
+        res.bonds = [bond.indices(res.ffmol) for bond in res.ffmol.bonds]
+        res.angles = [angle.indices(res.ffmol) for angle in res.ffmol.angles]
 
-    # Load the force field definitions
-    ff = elbalib.Elba()
-    ff.load(args.xml)
-
-    # Load a universe for the MD trajectory
-    if args.struct is None:
-        universe = md.Universe(args.ref, args.file)
-    else:
-        universe = md.Universe(args.struct, args.file)
-    # Load the reference CG universe
-    refuni = md.Universe(args.ref)
-
-    # Check if we need to translate the atom names
-    namtrans = None
-    if args.naming is not None:
-        namref = [line.strip() for line in open(args.naming[0], 'r').readlines()]
-        nammob = [line.strip() for line in open(args.naming[1], 'r').readlines()]
-        namtrans = {}
-        for nr, nm in zip(namref, nammob):
-            if nm != "*":
-                namtrans[nm] = nr
-
-    # Pre-processing
-    resids = []
-    mdresidues = []
-    bond_dict = {}
-    ang_dict = {}
-    trans_mats = {}
-    ffmols = {}
-    for r in refuni.selectAtoms("all").residues:
-        ffmol = ff.find_molecule(r.name)
-        if ffmol is None or not (ffmol.bonds and ffmol.angles):
-            continue
-        resid = "resid %d" % r.id
-        resids.append(resid)
-        mdresidues.append(universe.selectAtoms(resid))
-        ffmols[resid] = ffmol
-
-        # Setup bond and angle selections that are to be tracked
-        bond_dict[resid] = [bond.indices(ffmol) for bond in ffmol.bonds]
-        ang_dict[resid] = [angle.indices(ffmol) for angle in ffmol.angles]
-
-        # Setup transformation matrices if necessary
-        if args.struct is not None:
-            if namtrans is None:
-                atomnames = [a.name.lower() for a in mdresidues[-1]]
-            else:
-                atomnames = [namtrans[a.name].lower() for a in mdresidues[-1]]
-            trans_mats[resid] = ffmol.transmat(atomnames)
-
-    # Open the output file in a context manager
-    with open(args.out, "w") as f:
-
-        # Loop over the coordinates
-        for ti, ts in enumerate(universe.trajectory):
-
-            if ti * args.dt % 1000 == 0:
-                print "%d" % (ti * args.dt),
-                sys.stdout.flush()
-            if ti * args.dt < args.skip:
-                continue
-
-            # Loop over all residues
-            for ri, (resid, mdres) in enumerate(zip(resids, mdresidues)):
-                # Get the coordinates, either out-of-the-box CG or transformed AA
-                if args.struct is None:
-                    xyz = mdres.get_positions()
-                else:
-                    xyz = np.dot(trans_mats[resid], mdres.get_positions())
-                # Print out bond and angle values
-                f.write("%d" % (ti * args.dt))
-                for i, bondsel in enumerate(bond_dict[resid]):
-                    f.write("%8.3f" % _calc_bond(xyz[bondsel, :]))
-                for i, angsel in enumerate(ang_dict[resid]):
-                    f.write("%8.3f" % _calc_angle(xyz[angsel, :]))
-                f.write("\n")
+    # Open the output file in a context manager and then process the trajectory
+    with open(processor.args.out, "w") as f:
+        processor.process(None,residuefunc=_process_res,fileobj=f)
+                
